c21abc35f69edf78aef71c2f0269b0f52ab878e0
Thunderbirda <AKIRA@MIMURAAKIRA-no-MacBook-Air.local>
2015-05-03 21:54:34 +0900

    Xcodeのプロジェクトをコミットします

---
+++ Practive_0503/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practive_0503
+//
+//  Created by MIMURAAKIRA on 2015/05/03.
+//  Copyright (c) 2015年 MIMURAAKIRA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Practive_0503Tests/Practive_0503Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Practive_0503Tests.swift
+//  Practive_0503Tests
+//
+//  Created by MIMURAAKIRA on 2015/05/03.
+//  Copyright (c) 2015年 MIMURAAKIRA. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Practive_0503Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
